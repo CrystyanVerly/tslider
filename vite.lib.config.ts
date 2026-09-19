c72fce064377bb032fd115fe7b25f5a9de17b386
unknown <crystyanknappverly@gmail.com>
2026-09-19 16:32:25 -0300

    chore: prepare package for npm release

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: (format) => (format === 'es' ? 'tslider.js' : 'tslider.cjs'),
-      cssFileName: 'style',
-    },
-  },
-  plugins: [dts({ rollupTypes: true })],
+	publicDir: false,
+	build: {
+		lib: {
+			entry: 'src/index.ts',
+			formats: ['es', 'cjs'],
+			fileName: (format) => (format === 'es' ? 'tslider.js' : 'tslider.cjs'),
+			cssFileName: 'style',
+		},
+	},
+	plugins: [
+		dts({
+			exclude: ['src/main.ts'],
+		}),
+	],
 });
